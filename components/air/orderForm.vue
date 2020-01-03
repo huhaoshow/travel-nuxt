190f39fda3f55731ec3e9d32c5a8aa0f7bff27fc
huhaoshow <huhaomine@163.com>
2020-01-03 18:33:18 +0800

    完成了订单列表的渲染

---
+++ components/air/orderForm.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="main">
+    <div class="air-column">
+      <h2>乘机人</h2>
+      <el-form class="member-info">
+        <div class="member-info-item">
+          <el-form-item label="乘机人类型">
+            <el-input
+              placeholder="姓名"
+              class="input-with-select"
+              v-model="users[0].username"
+            >
+              <el-select v-model="type" slot="prepend" value="1" placeholder="请选择">
+                <el-option label="成人" value="1"></el-option>
+                <el-option label="儿童" value="2"></el-option>
+              </el-select>
+            </el-input>
+          </el-form-item>
+
+          <el-form-item label="证件类型">
+            <el-input v-model="users[0].id" placeholder="证件号码" class="input-with-select">
+              <el-select v-model="idType" slot="prepend" value="1" placeholder="请选择">
+                <el-option label="身份证" value="1" :checked="true"></el-option>
+                <el-option label="军人身份证" value="2" :checked="true"></el-option>
+              </el-select>
+            </el-input>
+          </el-form-item>
+
+          <span class="delete-user" @click="handleDeleteUser()">-</span>
+        </div>
+      </el-form>
+
+      <el-button class="add-member" type="primary" @click="handleAddUsers">添加乘机人</el-button>
+    </div>
+
+    <div class="air-column">
+      <h2>保险</h2>
+      <div>
+        <div class="insurance-item">
+          <el-checkbox label="航空意外险：￥30/份×1  最高赔付260万" border></el-checkbox>
+        </div>
+      </div>
+    </div>
+
+    <div class="air-column">
+      <h2>联系人</h2>
+      <div class="contact">
+        <el-form label-width="60px">
+          <el-form-item label="姓名">
+            <el-input v-model="contactName"></el-input>
+          </el-form-item>
+
+          <el-form-item label="手机">
+            <el-input v-model="contactPhone" placeholder="请输入内容">
+              <template slot="append">
+                <el-button @click="handleSendCaptcha">发送验证码</el-button>
+              </template>
+            </el-input>
+          </el-form-item>
+
+          <el-form-item label="验证码">
+            <el-input v-model="captcha"></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="warning" class="submit" @click="handleSubmit">提交订单</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 数据
+  data() {
+    return {
+      users: [
+        {
+          username: "", //  乘车人姓名
+          id: "" // 乘车人身份证
+        }
+      ],
+      insurances: [], // 保险id
+      contactName: "", // 联系人姓名
+      contactPhone: "", // 联系人电话
+      captcha: "", // 手机验证码
+      invoice: false, // 是否需要发票
+      seat_xid: "", // 座位id
+      air: "", // 航班id
+      type: "", // 乘机人类型
+      idType: "" // 证件类型
+    };
+  },
+  // 方法函数
+  methods: {
+    // 添加乘机人
+    handleAddUsers() {},
+
+    // 移除乘机人
+    handleDeleteUser() {},
+
+    // 发送手机验证码
+    handleSendCaptcha() {},
+
+    // 提交订单
+    handleSubmit() {
+      console.log(this.formData);
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.air-column {
+  border-bottom: 1px #ddd dashed;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+}
+
+.air-column h2 {
+  margin-bottom: 20px;
+  font-size: 22px;
+  font-weight: normal;
+}
+
+/deep/ .el-select .el-input {
+  width: 130px;
+}
+
+.input-with-select {
+  width: 590px;
+}
+
+.input-with-select /deep/ .el-input-group__prepend {
+  background-color: #fff;
+}
+.member-info /deep/ .el-form-item {
+  margin-bottom: 0;
+}
+
+.member-info-item {
+  border-bottom: 1px #eee dashed;
+  padding-bottom: 20px;
+  position: relative;
+
+  &:first-child {
+    .delete-user {
+      display: none;
+    }
+  }
+}
+
+.add-member {
+  margin-top: 20px;
+}
+
+.delete-user {
+  display: block;
+  background: #ddd;
+  width: 16px;
+  height: 16px;
+  font-size: 14px;
+  text-align: center;
+  line-height: 16px;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 50px;
+  position: absolute;
+  right: -30px;
+  top: 50%;
+}
+
+.insurance {
+  > div {
+    margin-top: 10px;
+  }
+}
+
+.insurance-item {
+  margin-bottom: 20px;
+}
+
+.contact {
+  /deep/ .el-input {
+    width: 50%;
+  }
+}
+
+.submit {
+  margin: 50px auto;
+  display: block;
+  width: 250px;
+  height: 50px;
+}
+</style>
---
+++ pages/air/order.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <div class="main">
+          <OrderForm />
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// 引入订单表单组件
+import OrderForm from '@/components/air/orderForm'
+export default {
+    // 注册组件
+    components:{OrderForm}
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
+</style>
